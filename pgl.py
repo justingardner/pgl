854780c1ec2324d5b65f418df162bd6df40258bd
Justin Gardner <jlg@stanford.edu>
2025-07-07 11:05:44 -0700

    Adding pgl.py which is the main class for running pgl

---
+++ pgl.py
@@ -0,0 +1,12 @@
+################################################################
+# # pgl.py: Main module for the pgl psychophysics and experiment library
+################################################################
+class pgl:
+    # Init Function
+    def __init__(self):
+        print("pgl instance created")
+
+    # Test function
+    def helloworld(self):
+        print("(pgl) Hello World!")
+    
