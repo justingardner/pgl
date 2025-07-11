591f4cd0b054e6f409cd1b51c5e55cbfec2cc078
Justin Gardner <jlg@stanford.edu>
2025-07-10 18:47:53 -0700

    refactoring code to use mixin classes, so that we avoid having one very long file with everything in it. SPlit pglBase and pglResolution

--- pgl/__init__.py
+++ pgl/__init__.py
@@ -1,3 +1,7 @@
-from .pgl import pgl
+from .pglBase import pglBase
+from .pglResolution import pglResolution
 from .screen import screen
 from .task import task
+
+class pgl(pglBase, pglResolution):
+    pass
