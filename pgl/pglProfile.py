c3c492581f88a9b678860e946f81db511924d686
Justin Gardner <jlg@stanford.edu>
2025-07-22 14:02:27 -0700

    Updated graph of profile information with more detailed information including saving some info about the state of the profile when it happened, what the screen was etc

--- pgl/pglProfile.py
+++ pgl/pglProfile.py
@@ -11,6 +11,7 @@
 #############
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 #############
 # Profile class
@@ -28,6 +29,7 @@ class pglProfile:
     profileModeFlushBuffer = None
     profileModeBufferIndex = 0
     profileList = []
+    profileInfo = {}
 
     ################################################################
     # profileMode property
@@ -47,10 +49,11 @@ class pglProfile:
 
         # If profileMode is set
         if self._profileMode > 0:
+            # get frameRate of screen
+            _,_,frameRate,_ = self.getResolution()
             # set default profileModeBufferSize if not set
             if self.profileModeBufferSize is None:
                 # get frameRate
-                _,_,frameRate,_ = self.getResolution()
                 # set buffer size to 60 seconds worth of frames
                 self.profileModeBufferSize = int(frameRate * 60)
                 # print the buffer size
@@ -60,15 +63,34 @@ class pglProfile:
                 # init buffer FIX, FIX, FIX deal with profile mode = 2 here
                 self.profileModeFlushBuffer = np.zeros(self.profileModeBufferSize)
                 self.profileModeBufferIndex = 0
+            # initialize information about the profile
+            self.profileInfo['startTime'] = time.time()
+            localTime = time.localtime(self.profileInfo['startTime'])
+            self.profileInfo['startTimeStr'] = time.strftime("%Y-%m-%d %H:%M:%S", localTime)
+
+            self.profileInfo['profileMode'] = self._profileMode
+            self.profileInfo['profileModeBufferStartSize'] = self.profileModeBufferSize
+            self.profileInfo['frameRate'] = frameRate
+            # save screen info
+            self.getWindowFrameInDisplay()
+            self.profileInfo['whichScreen'] = self.whichScreen
+            self.profileInfo['screenX'] = self.screenX
+            self.profileInfo['screenY'] = self.screenY
+            self.profileInfo['screenWidth'] = self.screenWidth
+            self.profileInfo['screenHeight'] = self.screenHeight
         else:
             # If profileMode is set to off, then save the profile information
             if self.profileModeBufferIndex > 0:
                 print("(pglProfile) profileMode is off, saving profile data.")
+                # set end time
+                self.profileInfo['endTime'] = time.time()
+                localTime = time.localtime(self.profileInfo['endTime'])
+                self.profileInfo['endTimeStr'] = time.strftime("%H:%M:%S", localTime)
+
+                # package into the dict
+                self.profileInfo['flushTimes'] = self.profileModeFlushBuffer[:self.profileModeBufferIndex-1]
                 # Save the profile information to the profileList
-                self.profileList.append(self.profileModeFlushBuffer[:self.profileModeBufferIndex-1])
-                # update default buffer size if we went over
-                if  self.profileModeBufferIndex > self.profileModeBufferSize
-                    self.profileModeBufferSize = self.profileModeBufferIndex
+                self.profileList.append(self.profileInfo)
                 # Reset the buffer index
                 self.profileModeBufferIndex = 0
 
@@ -85,17 +107,57 @@ class pglProfile:
         if len(self.profileList) <= 0:
             print("(pglProfile) No profile data available.")
         else:
-            for iProfile, profile in enumerate(self.profileList):
+            for iProfile, profileInfo in enumerate(self.profileList):
                 # display the number of frames in the profile
-                print(f"(pglProfile) Profile {iProfile+1}: {len(profile)} frames")
+                print(f"-------- pglProfile {iProfile+1} -------------")
+                flushTimes = profileInfo['flushTimes']
+                nFrames = len(flushTimes)
+                totalTime = flushTimes[-1] - flushTimes[0]
+                expectedFrameTime = 1 / profileInfo['frameRate']*1000  # in ms
                 # get the difference in times
-                frameTimes = np.diff(profile)
+                frameTimes = np.diff(flushTimes)
                 # compute stats
                 meanFrameTime = np.mean(frameTimes)
+                medianFrameTime = np.median(frameTimes)
                 stdFrameTime = np.std(frameTimes)
-                droppedFrames = np.sum(frameTimes > meanFrameTime + meanFrameTime/2)
-                # display the mean and std of the frame times
-                print(f"(pglProfile) Mean frame time: {meanFrameTime*1000:.2f} ms, std: {stdFrameTime*1000:.2f} ms")
-                print(f"(pglProfile) Dropped frames: {droppedFrames}")  
+                dropCriteria = meanFrameTime + meanFrameTime/2
+                droppedFrames = np.sum(frameTimes > dropCriteria)
+                # add one frames worth of time to totalTime (since we calculated the difference from the end of first to last, but didnt include the first frame
+                totalTime += expectedFrameTime / 1000
+                # format info for display
+                profileText = f"{nFrames} frames, {totalTime:0.3f} secs Screen: {profileInfo['whichScreen']} ({profileInfo['screenWidth'].pix}x{profileInfo['screenHeight'].pix})"
+                timeText = f"Started: {profileInfo['startTimeStr']} Ended: {profileInfo['endTimeStr']}"
+                frameText = f"Frame Rate: {profileInfo['frameRate']} Hz Expected Frame Time: {expectedFrameTime:.2f} ms"
+                frameTimeText = f"Median frame time: {medianFrameTime*1000:.2f} ms, {meanFrameTime*1000:.2f} ± {stdFrameTime*1000:.2f} mean ± std ms"
+                droppedFramesText = f"Dropped frames (longer than {dropCriteria*1000:0.2f} ms): {droppedFrames} ({droppedFrames/nFrames*100:.2f}%)"
+                # print information
+                print(profileText)
+                print(timeText)
+                print(frameText)
+                print(frameTimeText)
+                print(droppedFramesText)
                 # plot a historgram
-                plt.hist(frameTimes, bins=50, alpha=0.75, edgecolor='black')
+                counts, _, _ = plt.hist(frameTimes*1000, bins=50, alpha=0.75, edgecolor='black')
+                plt.title(f'Frame Time Histogram\n{profileText}\n{timeText}\n{frameText}\n{frameTimeText}\n{droppedFramesText}')
+                plt.xlabel('Frame Time (ms)')
+                plt.ylabel('Count (n)')
+                plt.grid(axis='y', alpha=0.75)
+
+                # Annotate with an arrow and line
+                plt.axvline(expectedFrameTime, color='red', linestyle='dashed', linewidth=1)
+                plt.annotate('Expected Frame Time',
+                    xy=(expectedFrameTime, max(counts)*0.9),  # The point (expectedFrameTime, y_value)
+                    xytext=(expectedFrameTime + 1, max(counts)*0.9),  # The text position
+                    horizontalalignment='left',
+                    verticalalignment='center',
+                    arrowprops=dict(facecolor='red', shrink=0.05))
+                plt.show()
+    ################################################################
+    # profileModeClearAll
+    ################################################################
+    def profileModeClearAll(self):
+        """
+        Clear all profile data.
+        """
+        self.profileList = []
+        print("(pglProfile) Cleared all profile data.")
