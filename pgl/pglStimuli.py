d9049b6de5111f3cb4a30a802908a0fdfbbb8ae5
Justin Gardner <jlg@stanford.edu>
2025-07-26 23:28:16 -0700

    Adding gabor stimulus, also setting up a stimulus that will handle creating and deleting textures and displaying a set of images in sequence, still debugging this

--- pgl/pglStimuli.py
+++ pgl/pglStimuli.py
@@ -134,3 +134,115 @@ class pglStimuli:
 
         return gaussian
     
+    def gabor(self, width=None, height=None, spatialFrequency=1.0, orientation=0.0, contrast=1.0, phase=0.0, stdX=None, stdY=None):
+        '''
+        Generate a Gabor patch.
+
+        Parameters:
+        - spatialFrequency: Frequency of the grating in cycles per degree.
+        - phase: Phase of the grating in degrees.
+        - orientation: Orientation of the grating in degrees.
+        - contrast: Contrast of the grating (0 to 1).
+        - width: Width of the Gabor patch in degrees (default is screenWidth).
+        - height: Height of the Gabor patch in degrees (default is screenHeight).
+        - stdX: Standard deviation in the X direction (default is 1/8 width).
+        - stdY: Standard deviation in the Y direction (default is 1/8 height).
+
+        Returns:
+        - A numpy array representing the Gabor stimulus.
+        '''
+        if self.coordinateFrame != "visualAngle":
+            print("(pgl:pglStimuli:gabor) Error: gabor can only be generated in visualAngle coordinates. run visualAngle() ")
+            return None
+
+        # Validate inputs
+        if not isinstance(spatialFrequency, (int, float)) or spatialFrequency <= 0:
+            print("(pgl:pglStimuli:gabor) spatialFrequency must be a positive number.")
+            return None
+        if not isinstance(phase, (int, float)):
+            print("(pgl:pglStimuli:gabor) phase must be a number.")
+            return None
+        if not isinstance(orientation, (int, float)):
+            print("(pgl:pglStimuli:gabor) orientation must be a number.")
+            return None
+        if not isinstance(contrast, (int, float)) or not (0 <= contrast <= 1):
+            print("(pgl:pglStimuli:gabor) contrast must be a number between 0 and 1.")
+            return None 
+
+        # create the gabor
+        grating = self.grating(width, height, spatialFrequency, orientation, contrast, phase)
+        gaussian = self.gaussian(width, height, stdX, stdY)
+        gabor = ((grating * gaussian)+1)/2
+
+        # create a stimulus
+        gaborStimulus = pglImageStimulus(self)
+        rgba = np.zeros((gabor.shape[0], gabor.shape[1], 4), dtype=np.float32)
+        rgba[..., 0] = gabor
+        rgba[..., 1] = gabor
+        rgba[..., 2] = gabor
+        rgba[..., 3] = gaussian
+        gaborStimulus.addImage(rgba)
+        return gaborStimulus
+class _pglStimulus:
+    '''
+    Base class for all stimuli.
+    This class is not meant to be instantiated directly.
+    '''
+    pgl = None
+    def __init__(self, pgl):
+        self.pgl = pgl
+
+    def display(self):
+        '''
+        Display the stimulus.
+        This method should be implemented by subclasses.
+        '''
+        raise NotImplementedError("Subclasses must implement this method.")
+
+class pglImageStimulus(_pglStimulus):
+    '''
+    Base class for image stimuli.
+    This class is not meant to be instantiated directly.
+    '''
+    nImages = 0
+    currentImage = 0
+    imageList = []
+    def __init__(self, pgl):
+        '''
+        Initialize the image stimulus with an image instance.
+        
+        Args:
+        '''
+        super().__init__(pgl)
+    
+    def addImage(self, imageData):
+        '''
+        Add an image to the stimulus.
+        
+        Args:
+        - imageData: The image data to be added.
+        '''
+        if not isinstance(imageData, np.ndarray) or imageData.ndim != 3 or imageData.shape[2] != 4:
+            print("(pgl:pglStimulus:addImage) Error: imageData must be a nxmx4 numpy matrix.")
+            return None
+        
+        # make into a pgl image
+        imageInstance = self.pgl.imageCreate(imageData)
+        self.imageList.append(imageInstance)
+        # update count
+        self.nImages += 1
+
+    def display(self):
+        '''
+        Display the current image.
+        '''
+        if self.nImages == 0:
+            print("(pgl:pglStimulus:display) Error: No images to display.")
+            return None
+
+        currentImage = self.imageList[self.currentImage]
+        currentImage.display()
+        currentImage.print()
+        # Increment the current image index
+        self.currentImage = (self.currentImage + 1) % self.nImages
+        print(f"(pgl:pglStimulus:display) Displaying image {self.currentImage} of {self.nImages}.")
