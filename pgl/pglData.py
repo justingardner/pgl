134781e56c80e629c7340ad60c96a28b2b7b5e09
Justin Gardner <jlg@stanford.edu>
2026-02-18 22:59:28 -0800

    working on staircase to add serializable settings and serializable data. Not yet working version

---
+++ pgl/pglData.py
@@ -0,0 +1,17 @@
+################################################################
+#   filename: pglData.py
+#    purpose: Classes that handle serializing and deserializing data
+#         by: JLG
+#       date: Feb 18, 2026
+################################################################
+
+##############
+# import
+##############
+
+def pglData:
+    '''
+    Module for pglData classes that
+    provides methods for loading/saving from JSON
+    '''
+    
