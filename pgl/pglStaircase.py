16a0f04b63e4c0726df6c8cd52999647cf4832d7
Justin Gardner <jlg@stanford.edu>
2026-02-19 23:24:57 -0800

    working version of staircase which runs a simulated observer and correctly does a 2-down 1-up staircase with adaptive step sizes

--- pgl/pglStaircase.py
+++ pgl/pglStaircase.py
@@ -12,9 +12,9 @@ import numpy as np
 import itertools
 import random
 from . import pglParameter
+import matplotlib.pyplot as plt
 from .pglTimestamp import pglTimestamp
-from .pglSettings import pglSerializable
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from traitlets import Float, Int, List
 from .pglData import pglData
 from .pglSettings import _pglSettings
@@ -32,12 +32,12 @@ class pglStaircase():
         Initialize the staircase, by setting up structures to save data. this
         should be called by the subclass after the parameters are set.
         '''
-        # save the description
+        # init list of all data
+        self.allData = []
+        # save description
         self.description = description
-        # history of staircases, each staircase is an entry in this list
-        self.history = []
-        # initialize the first staircase
-        self.startStaircase()
+        # timestamp object for getting times
+        self.pglTimestamp = pglTimestamp()
         
     def get(self):
         '''
@@ -52,10 +52,40 @@ class pglStaircase():
         then call this method to store the value and response
         in the history.
         '''
-        self.history[-1]["values"].append(value)
-        self.history[-1]["responses"].append(correct)
-        self.history[-1]["nTrials"] += 1
+        self.data.values.append(value)
+        self.data.responses.append(correct)
+        self.data.nTrials += 1
+    
+    def finished(self):
+        '''
+        returns whether the staircase should finish. Subclasses
+        should implement this if needed.
+        '''
+        return False
+        
+    def startStaircase(self, startVal=None):
+        '''
+        start a staircase, initializes the history. For subclasses,
+        this should be called after you initialize the staircase parameters
+        so they get stored in the history correctly.
+        '''
+        # initialize data
+        self.data = pglStaircaseData()
+        # save in our list of all data
+        self.allData.append(self.data)
+        # save the description
+        self.data.description = self.description
 
+        # add the start time
+        self.data.startTime = self.pglTimestamp.getSecs()
+
+    def endStaircase(self):
+        '''
+        call to end the staircase
+        '''
+        # add end time
+        self.data.endTime = self.pglTimestamp.getSecs()
+        
     def getThresholdEstimate(self):
         '''
         Get the current threshold estimate of the staircase parameter.
@@ -66,26 +96,9 @@ class pglStaircase():
         '''
         Plot the history of the staircase parameter.
         '''
-        pass
-    
-    def startStaircase(self, startVal=None):
-        '''
-        start a staircase, initializes the history. For subclasses,
-        this should be called after you initialize the staircase parameters
-        so they get stored in the history correctly.
-        '''
-        # Each entry of the history represents a staircase run
-        # we first save all the values of the staircase to the history
-        self.history.append(self.__dict__.copy())
-        
-        # add the start time
-        self.history[-1]["startTime"] = self.pglTimestamp.getSecs()
-        
-        # initialize values and responses
-        self.history[-1]["values"] = []
-        self.history[-1]["responses"] = []
-        self.history[-1]["nTrials"] = 0
-
+        ax = self.data.plot()
+        return ax
+            
     def test(self, observerModel=None, nTrials=100, minVal=0.0, maxVal=100.0, threshold=7.5, slope=3.0):
         '''
         Test the staircase by running a simple simulation.
@@ -93,13 +106,13 @@ class pglStaircase():
         
         # get an observer model
         if observerModel is None:
-            observerModel = pglObsserverModel(threshold=threshold, slope=slope)
+            observerModel = pglObserverModel(threshold=threshold, slope=slope)
 
         # start a new staircase
         self.startStaircase()
         
         # run the staircase for nTrials
-        for trial in range(nTrials):
+        while not self.finished() and (self.data.nTrials < nTrials):
             # get the current stimulus value from the staircase
             stimulus = self.get()
             # 2AFC response comparing stimulus to 0
@@ -107,7 +120,18 @@ class pglStaircase():
             # update the staircase
             self.update(stimulus, response)
 
-        return self.getThresholdEstimate()
+        # end the staircase
+        self.endStaircase()
+        
+        # plot the results
+        ax = self.plot()
+        
+        # plot a horizontal line at the threshold
+        trueThreshold = threshold*(-np.log(1-0.707))**(1/slope)
+        ax.axhline(y=threshold, color='g', linestyle='--', label='True Threshold')
+        ax.legend()
+        
+        return
 
 ##########################
 # Observer model class
@@ -123,7 +147,7 @@ class pglObserverModel():
         self.threshold = threshold
         self.slope = slope
     
-    def get2AFCResponse(self, stimulusValue1, stimulustValue2):
+    def get2AFCResponse(self, stimulusValue1, stimulusValue2):
         '''
         Get the response of the observer model to a given stimulus value.
         Response is coded as 1 for correct, 0 for incorrect
@@ -148,23 +172,26 @@ class pglStaircaseUpDown(pglStaircase):
     to an experiment like other parameters. It does 
     '''
     def __init__(self, nUp=2, nDown=1, stepSize=1,
-                 startVal=50.0, minVal=0.0, maxVal=100.0,
-                 stepChangeSizes=[0.5, 0.25, 0.125], stepChangeReversals=[3, 6, 9],
-                 description=""):
+                 startVal=20.0, minVal=0.0, maxVal=100.0,
+                 stepChangeSizes=[0.5, 0.25, 0.125], stepChangeReversals=[2, 4, 6],
+                 numReversalsToFinish=12,
+                 description="upDownStaircase"):
         '''
         Initialize the up-down staircase 
         '''
 
         # staircase parameters
-        self.nUp = nUp
-        self.nDown = nDown
-        self.stepSize = stepSize
-        self.startVal = startVal
-        self.minVal = minVal
-        self.maxVal = maxVal
-        self.stepChangeSizes = stepChangeSizes
-        self.stepChangeReversals = stepChangeReversals
-
+        self.settings = pglStaircaseSettings()
+        self.settings.nUp = nUp
+        self.settings.nDown = nDown
+        self.settings.stepSize = stepSize
+        self.settings.startVal = startVal
+        self.settings.minVal = minVal
+        self.settings.maxVal = maxVal
+        self.settings.stepChangeSizes = stepChangeSizes
+        self.settings.stepChangeReversals = stepChangeReversals
+        self.settings.numReversalsToFinish = numReversalsToFinish
+        
         # run init to start the staircase history (this needs to come
         # after the parameters are set)
         super().__init__(description)
@@ -175,19 +202,15 @@ class pglStaircaseUpDown(pglStaircase):
         '''
 
         # initialize current value
-        self.currentVal = self.startVal
+        self.currentVal = self.settings.startVal
 
         # initialize other variables
         self.correctStreak = 0
         self.incorrectStreak = 0
-        self.reversals = 0
         self.lastDirection = None  
 
-        # call superclass method to set up history
+        # call superclass method to initialize data
         super().startStaircase(startVal)
-        
-        # add reversals to the dictionary
-        self.history[-1]["reversals"] = []
         
     def get(self):
         '''
@@ -211,59 +234,84 @@ class pglStaircaseUpDown(pglStaircase):
             self.correctStreak = 0
 
         # check for streaks that trigger a change
-        if self.correctStreak == self.nUp:
-            # go up
-            self.currentVal += self.stepSize
+        if self.correctStreak == self.settings.nUp:
+            # go down
+            self.currentVal -= self.settings.stepSize
             # clamp to max
-            if self.currentVal > self.maxVal: self.currentVal = self.maxVal
+            if self.currentVal > self.settings.maxVal: self.currentVal = self.settings.maxVal
             # reset streak
             self.correctStreak = 0
             # check for reversal
-            if self.lastDirection == "down":
-                self.reversals += 1
-                self.history[-1]["reversals"].append(1)
-            else:
-                self.history[-1]["reversals"].append(0)
+            if self.lastDirection == "up":
+                self.data.reversals.append(self.data.nTrials)
+                self.updateStepSize()
             # remember the last direction
-            self.lastDirection = "up"
-        elif self.incorrectStreak == self.nDown:
-            # go down
-            self.currentVal -= self.stepSize
+            self.lastDirection = "down"
+        elif self.incorrectStreak == self.settings.nDown:
+            # go up
+            self.currentVal += self.settings.stepSize
             # clamp to min
-            if self.currentVal < self.minVal: self.currentVal = self.minVal
+            if self.currentVal < self.settings.minVal: self.currentVal = self.settings.minVal
             # reset streak
             self.incorrectStreak = 0
             # check for reversal
-            if self.lastDirection == "up":
-                self.reversals += 1
-                self.history[-1]["reversals"].append(1)
-            else:
-                self.history[-1]["reversals"].append(0)
+            if self.lastDirection == "down":
+                self.data.reversals.append(self.data.nTrials)
+                self.updateStepSize()
             # remember the last direction
-            self.lastDirection = "down"
+            self.lastDirection = "up"
+    
+    def updateStepSize(self):
+        # get number of reversals
+        numReversals = len(self.data.reversals)
+
+        # loog in stepChangeReversals for a match
+        for revThreshold, stepSize in zip(
+            self.settings.stepChangeReversals,
+            self.settings.stepChangeSizes
+        ):
+            # if there is a match then set stepsize
+            if numReversals >= revThreshold:
+                self.settings.stepSize = stepSize
+
+    def finished(self):
+        '''
+        returns whether the staircase should finish. 
+        '''
+        if len(self.data.reversals) >= self.settings.numReversalsToFinish:
+            return True
+
             
 @dataclass
-pglStaircaseData(pglData):
+class pglStaircaseData(pglData):
     '''
     Class representing the data from a staircase.
     This is used to save/load staircase data.
     '''
+    description: str = ""
     startTime: float = 0.0
-    values: np.ndarray = field(default_factory=lambda: np.array([]))
-    responses: np.ndarray = field(default_factory=lambda: np.array([]))
+    values: list = field(default_factory=list)
+    responses: list = field(default_factory=list)
     nTrials: int = 0
-    reversals: list = field(default_factory=list)
+    reversals: list = field(default_factory=list)  
     
-
-    
-    def __init__(self):
+    def plot(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
         '''
-        Initialize the staircase data.
+        Plot the staircase data.
         '''
-        self.history = []
+        ax.plot(self.values, marker='o')
+        ax.set_title(f'Staircase Data: {self.description}')
+        ax.set_xlabel('Trial')
+        ax.set_ylabel('Staircase Value')
+        for rev in self.reversals:
+            ax.axvline(x=rev, color='r', linestyle='--', label='Reversal' if rev == self.reversals[0] else "")
+        if self.reversals:
+            ax.legend()
+        return ax
 
-
-pglStaircaseData(_pglSettings):
+class pglStaircaseSettings(_pglSettings):
     nDown = Int(2, min=0, step=1, help="Number of trials in a row before increasing difficulty")
     nUp = Int(1, min=0, step=1, help="Number of trials in a row before increasing difficulty")
     startVal = Float(default_value=None, allow_none=True, help="Starting value for the staircase")
@@ -272,3 +320,4 @@ pglStaircaseData(_pglSettings):
     stepSize = Float(1.0, min=0.0, help="Step size for the staircase")
     stepChangeSizes = List(Float(), default_value=[0.5, 0.25, 0.125], help="Step sizes to change to at each reversal count")
     stepChangeReversals = List(Int(), default_value=[2, 4, 6], help="Reversal counts at which to change step size")
+    numReversalsToFinish = Int(8, min=1, help="Number of reversals at which the staircase is considered to have converged and should therefore finish")
