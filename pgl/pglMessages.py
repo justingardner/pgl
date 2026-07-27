0c170e32b3563f9417c2dbbb882aed6f5a3f0559
Justin Gardner <jlg@stanford.edu>
2026-07-27 00:10:02 -0700

    Updating to give name of mixin/parent class where function is defined

--- pgl/pglMessages.py
+++ pgl/pglMessages.py
@@ -45,10 +45,10 @@ class pglMessages:
         # print the warning
         cls.warning(msg=msg, level=level, callerNameDepth=3)
        
-    @staticmethod 
+    @staticmethod
     def getCallerName(depth=2):
         """
-        depth=1 -> caller of getCaller()
+        depth=1 -> caller of getCallerName()
         depth=2 -> caller of the caller
         etc.
         """
@@ -59,18 +59,55 @@ class pglMessages:
 
         functionName = frame.f_code.co_name
 
+        runtimeClass = None
+        definedClass = None
+
+        # Find runtime class
         if "self" in frame.f_locals:
-            className = frame.f_locals["self"].__class__.__name__
+            obj = frame.f_locals["self"]
+            runtimeClass = obj.__class__
+
         elif "cls" in frame.f_locals:
-            className = frame.f_locals["cls"].__name__
-        else:
-            className = None
+            obj = frame.f_locals["cls"]
+            runtimeClass = obj
 
-        if className:
-            return f"{className}:{functionName}"
-        else:
-            return functionName
+        # Find defining class using MRO
+        if runtimeClass is not None:
+            for cls in runtimeClass.mro():
+                if functionName in cls.__dict__:
+                    method = cls.__dict__[functionName]
 
-        
-        
-        
+                    if hasattr(method, "__code__"):
+                        if method.__code__ is frame.f_code:
+                            definedClass = cls
+                            break
+
+                    # handle classmethod / staticmethod
+                    elif hasattr(method, "__func__"):
+                        if method.__func__.__code__ is frame.f_code:
+                            definedClass = cls
+                            break
+
+        # Format output
+        if runtimeClass and definedClass:
+            if runtimeClass == definedClass:
+                return f"{runtimeClass.__name__}:{functionName}"
+            else:
+                return f"{runtimeClass.__name__}->{definedClass.__name__}:{functionName}"
+
+        elif runtimeClass:
+            return f"{runtimeClass.__name__}:{functionName}"
+
+        else:
+            moduleName = frame.f_globals.get("__name__", None)
+
+            if functionName == "<module>":
+                if moduleName and moduleName != "__main__":
+                    return moduleName.split(".")[-1]
+                else:
+                    return "<jupyter:script>"
+            else:
+                if moduleName and moduleName != "__main__":
+                    return f"{moduleName.split('.')[-1]}:{functionName}"
+                else:
+                    return functionName
